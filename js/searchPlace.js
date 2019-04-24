f33a90842d059f62beaf1f706d842ec665c60a4c
Felix Seol <felixxseol@gmail.com>
2019-04-24 16:30:13 +0900

    Search place with keyword.

--- js/searchPlace.js
+++ js/searchPlace.js
@@ -1,6 +1,6 @@
-function searchPlace(query){
+function searchPlace(_keyword){
     return new Promise(function (resolve, reject) {
-        fetch(`http://165.194.35.214:13000`)
+        fetch(`http://165.194.35.214:13000/?keyword=${_keyword}`)
             .then(function(res) {
                 // Response as Promise
                 res.json().then(body =>{
