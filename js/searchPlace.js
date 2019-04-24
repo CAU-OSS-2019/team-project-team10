5bd5249da3c00d3ee23ee10b6795126e09ac7bad
Felix Seol <felixxseol@gmail.com>
2019-04-24 14:41:48 +0900

    Get places data from WSWMexpress successfully.

--- js/searchPlace.js
+++ js/searchPlace.js
@@ -1,18 +1,16 @@
 function searchPlace(query){
-    // template literal
-    fetch(`https://naveropenapi.apigw.ntruss.com/map-place/v1/search?query=${query}&coordinate=127.1054328,37.3595963`, {
-        headers: {
-            "X-NCP-APIGW-API-KEY-ID":"sx4y6v49bc",
-            "X-NCP-APIGW-API-KEY":"KyJQ1dl1gU6an8IalWdW428EmOYChrwzS30GEtVP"
-        }
-    }).then(function(res) {
-        console.log(res);
-        // if (res.ok) {
-        //     alert("Perfect! Your settings are saved.");
-        // } else if (res.status == 401) {
-        //     alert("Oops! You are not authorized.");
-        // }
-    }, function(e) {
-        alert("Error submitting form!");
-    });
+    return new Promise(function (resolve, reject) {
+        fetch(`http://165.194.35.214:13000`)
+            .then(function(res) {
+                // Response as Promise
+                res.json().then(body =>{
+                    var placesData = JSON.parse(body).places;
+                    console.log(placesData);
+                    resolve(placesData);
+                }).catch(error=>{
+                    console.error(error);
+                    reject("Error fetching.")
+                });
+            });
+    })
 }
