619b27f58dc95ebd11247b3877df78be2b975a7f
Felix Seol <felixxseol@gmail.com>
2019-04-26 13:36:50 +0900

    Change Express port 13000->26656

--- js/searchPlace.js
+++ js/searchPlace.js
@@ -1,14 +1,14 @@
 function searchPlace(_keyword){
     return new Promise(function (resolve, reject) {
-        fetch(`http://165.194.35.214:13000/?keyword=${_keyword}`)
+        fetch(`http://165.194.35.214:26656/?keyword=${_keyword}`)
             .then(function(res) {
                 // Response as Promise
                 res.json().then(body =>{
                     var placesData = JSON.parse(body).places;
                     console.log(placesData);
                     resolve(placesData);
-                }).catch(error=>{
-                    console.error(error);
+                }).catch(err=>{
+                    console.error(err);
                     reject("Error fetching.")
                 });
             });
