b70bd92e81f2f1ec4a450acb9e9570b2fb6e30a3
CJBJ1 <46565543+CJBJ1@users.noreply.github.com>
2019-05-06 13:06:52 +0900

    refactoring 'newPoint.js' add 'btnList.js'
    newPoint.js 리팩토링
    btnList.js 추가 (버튼관리용)

--- js/newPoint.js
+++ js/newPoint.js
@@ -1,68 +1,86 @@
-function newPoint(map,markerList){
-    naver.maps.Event.addListener(map, 'click', function(e) {
+function newPoint() {
+    naver.maps.Event.addListener(map, 'click', function (e) {
         var marker = new naver.maps.Marker({
             position: e.coord,
             map: map,
             clickable: true,
-            name: markerList.length,
-            title: '새 출발지',
-            visible : true
+            title: '출발지'
         });
-        naver.maps.Event.addListener(marker, "click", function(e) {
-            delPointByMarker(marker);
-        });
-        markerList.push(marker);
-        appendBymarker(markerList);
-        delPoint(markerList);
-        changeName();
+
+        clickedMarker(marker);
+        startList.push(marker);
+        appendByMarker();
     });
 }
 
-function delPoint(markerList){
-    var order;
-    $('.delbtn').click(function(){
-        order=$(this).attr('name');
-        markerList[order].setMap(null);
-        markerList[order].visible=false;
-        $(document.getElementsByName(order)).remove();
-    });
-
-}
-
-function appendBymarker(markerList){
-    let text = `<li name ='${markerList.length-1}' class='nav-item'>
-    <a class='nav-link'>
-    <span data-feather='file-text'></span>새 출발지
-    <button type="button" class='changebtn' name='${markerList.length-1}' style="height:auto;width:auto"
-    onclick="">변경
-    </button>
-    <button type="button" class='delbtn' name='${markerList.length-1}' style="height:auto;width:auto"
-    onclick="">삭제
-    </button></a></li>`
+function appendByMarker() {
+    let text = `<li class='nav-item'>
+    <a class='nav-link'>새 출발지 ${changeBtn()} ${delBtn()} </a></li>`;
     $('#addedPlace').append(text);
 }
 
-function delPointByMarker(marker){
-    marker.setMap(null);
-    marker.visible=false;
-    $(document.getElementsByName(marker.name)).remove();
+function setInfo(marker) {
+    var inner = `
+    <div class="iw_inner">
+    <h3> ${marker.title}</h3>
+    </div>`;
+
+    var infowindow = new naver.maps.InfoWindow({
+        content: inner,
+        name: marker.position
+    });
+
+    infoList.push(infowindow);
+    return infowindow;
 }
 
-function changeName(){
-    var order;
-    $('.changebtn').click(function(){
-        var par = $(this).parents('a');
-        var cloneEle = par.children();
-        par.text('이름 변경됨');
+function clickedMarker(marker) {
+    naver.maps.Event.addListener(marker, "click", function () {
+        var i = 0;
+        var ninfo;
+        for (i = 0; i < infoList.length; i++) {
+            if (infoList[i].getMap()) {
+                infoList[i].close();
+                break;
+            }
+        }
+        ninfo = setInfo(marker);
+        if (i == infoList.length - 1) {
+            ninfo.open(map, marker);
+        }
+        else if (infoList[i].name != ninfo.name) {
+            ninfo.open(map, marker);
+        }
+        else {
+            infoList.splice(i, 1);
+        }
+        i = 0;
+
     });
 }
 
-function eMarkerList(markerList){
-    var new_list=[];
-    for(var i=0;i<markerList.length;i++){
-        if(markerList[i].visible){
-            new_list.push(markerList[i]);
+function btnController() {
+    $('.delBtn').off().click(function () {
+        var cle = $(this).closest('li');
+        var i = cle.index();
+        startList[i].setMap(null);
+        for (var j = 0; j < infoList.length; j++) {
+            if (startList[i].position == infoList[j].name) {
+                infoList[j].close();
+                infoList.splice(j, 1);
+            }
         }
-    }
-   return new_list;
+        startList.splice(i, 1);
+        cle.remove();
+    });
+
+    $('.chnBtn').off().click(function () {
+        var cle = $(this).closest('a');
+        var i = $(this).closest('li').index();
+        var input = prompt('이름을 입력해주세요');
+        cle.text(input);
+        cle.append(' ' + changeBtn() + ' ' + delBtn());
+        startList[i].title = input;
+        setInfo(startList[i]);
+    });
 }
