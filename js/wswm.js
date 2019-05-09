a1b34761ed9fea7be40838e2026894e664b8d806
garden <khjvvv7@gmail.com>
2019-05-09 22:18:23 +0900

    findCenter from startPosition

--- js/wswm.js
+++ js/wswm.js
@@ -6,7 +6,6 @@ var resultPlace = [];
 var toDoList = ["삼겹살", "당구","쀍뛝뙑"]; // test 를 위한 임시 toDoList
 
 function wswm(){
-    console.log(result);
     var center = findCenter(); // 사용자들의 중간 위치 반환.
     //var center = [37.509218, 126.963359]; //test 를 위한 임시 center
     var a = wswm2(center);
@@ -21,9 +20,9 @@ function wswm2(cen){
     while(!meet){ //이 장소에서 최종적으로 만날 수 있는지?
         for(var i = 0; i < toDoList.length; i++){ // 할일 목록인 toDoList가 있다고 가정 예 ) toDoList = ["삼겹살", "노래방", "방탈출카페", "당구장"]
             var plac= toDoList[i];
-            //searchPlace(plac, coordinate);
-            //listPlace(i, plac, coordinate);// center위치에서 toDoList에 있는 것들을 서치함.
-            console.log(result);
+            searchPlace(plac, coordinate);
+            listPlace(i, plac, coordinate);// center위치에서 toDoList에 있는 것들을 서치함.
+
             if(result[i+1] == 0) {
                 subMeet = false; // 즉 하고싶은 리스트 중 검색 안되는것이 있다면 그 장소에서는 만나면 안됨.
                 searched[i] = false;
@@ -32,7 +31,7 @@ function wswm2(cen){
                 searched[i] = true;
             }
         }
-        console.log("만날까말까~");
+
         if(subMeet){
             meet = true; // 결론적으로 현재 center에서 만나면 된다.
             return cen;
