a705b641849c20951d930bcfbd3c7cc9777f61ce
Jaehooooooon <jaehoon572@naver.com>
2019-05-17 16:27:19 +0900

    AND OR option function in andOrApplied.js

--- js/wswm.js
+++ js/wswm.js
@@ -20,7 +20,7 @@ async function wswm2(cen){
     var coordinate = cen[0] + "," + cen[1];
 
     while(!meet){ //이 장소에서 최종적으로 만날 수 있는지?
-<<<<<<< HEAD
+
         for(var i = 0; i < toDoList.length; i++){ // 할일 목록인 toDoList가 있다고 가정 예 ) toDoList = ["삼겹살", "노래방", "방탈출카페", "당구장"]
             var plac= toDoList[i];
             await searchPlace(plac, coordinate, 1500).then(function (resolvedData){
@@ -37,50 +37,10 @@ async function wswm2(cen){
                 }
             //listPlace(i, plac, coordinate);// center위치에서 toDoList에 있는 것들을 서치함.
         }
-    
-        //의미 없는 AND, OR 제거
-        if (toDoAndOrList.length == toDoList.length)
-            toDoAndOrList.pop();
-        //AND, OR 조건 추가
-        //var allFalse = 0;
-        var andIndex = [];  //toDoAndOrList에서 AND의 index들을 담음
-        var boolTemp = false;
-        var andGroup = [];
-        if (toDoList.length == 1) { //toDo가 하나만 있을 때
-            subMeet = searched[0];
-        } else {
-            for (var i=0; i<toDoAndOrList.length; i++) {    //AND들을 기준으로 그룹을 나눔 - 그 그룹 내에는 OR만 존재, 모두 false 여야 false
-                if (toDoAndOrList[i] == 0)
-                    andIndex.push(i);
-            }
 
-            for (var i=0; i<andIndex.length; i++) { 
-                if (andIndex[i]==0) {   //첫번째 그룹
-                    andGroup[0] = searched[0];
-                } else  {   //중간 그룹들
-                    for (var j = andIndex[i-1]+1; j <= andIndex[i]; j++) {
-                        if (searched[j] == true)
-                            boolTemp = true;
-                    }
-                    andGroup[i] = boolTemp;
-                    boolTemp = false;
-                }               
-            }
-
-            for (var i = andIndex[andIndex.length-1]+1; i < searched.length; i++) { //마지막 그룹
-                if (searched[i] == true)
-                    boolTemp = true;
-            }
-            andGroup[andIndex.length] = boolTemp;
-            boolTemp = false;
-            //최종 andGroup 판별 - AND로만 이루어져 모두 true 여야 subMeet이 true
-            for (var i = 0; i < andGroup.length; i++) {
-                if (andGroup[i] == false)
-                    subMeet = false;
-            }
-        }
-        console.log("subMeet : "+subMeet);
-
+        // AND OR 옵션 적용
+        subMeet = andOrApplied();
+        
         if(subMeet){
             meet = true; // 결론적으로 현재 center에서 만나면 된다.
             return cen;
