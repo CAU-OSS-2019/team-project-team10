1fb95c9df863e4ba6ffbbb75b1c2146d75f88c82
garden <khjvvv7@gmail.com>
2019-05-21 21:25:03 +0900

    delete useless comment

--- js/wswm.js
+++ js/wswm.js
@@ -7,7 +7,6 @@ var newCenter=[];
 function wswm(){
     console.log(result);
     var center = findCenter(); // 사용자들의 중간 위치 반환.
-    //var center = [37.509218, 126.963359]; //test 를 위한 임시 center
     var a = wswm2(center);
     return a;
 }
@@ -21,7 +20,7 @@ async function wswm2(cen){
 
     while(!meet){ //이 장소에서 최종적으로 만날 수 있는지?
 
-        for(var i = 0; i < toDoList.length; i++){ // 할일 목록인 toDoList가 있다고 가정 예 ) toDoList = ["삼겹살", "노래방", "방탈출카페", "당구장"]
+        for(var i = 0; i < toDoList.length; i++){
             var plac= toDoList[i];
             await searchPlace(plac, coordinate, 1500).then(function (resolvedData){
                 result.push(resolvedData.length);
@@ -29,7 +28,7 @@ async function wswm2(cen){
 
             console.log(result);
                 if(result[i] == 0) {
-                    //subMeet = false; // 즉 하고싶은 리스트 중 검색 안되는것이 있다면 그 장소에서는 만나면 안됨. 잠시 주석처리!
+                    subMeet = false; // 즉 하고싶은 리스트 중 검색 안되는것이 있다면 그 장소에서는 만나면 안됨.
                     searched.push(false);
                 }
                 else{
