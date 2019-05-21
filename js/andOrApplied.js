b69441394bee54f283c35bd26e45234c34992e6e
Jaehooooooon <jaehoon572@naver.com>
2019-05-22 01:14:56 +0900

    andOr, 초기화  에러 수정

--- js/andOrApplied.js
+++ js/andOrApplied.js
@@ -8,13 +8,21 @@ function andOrApplied() {
     var andIndex = [];  //toDoAndOrList에서 AND의 index들을 담음
     var boolTemp = false;
     var andGroup = [];
-
+    
     if (toDoList.length == 1) { //toDo가 하나만 있을 때
         _subMeet = searched[0];
     } else {
         for (var i = 0; i < toDoAndOrList.length; i++) {    //AND들을 기준으로 그룹을 나눔 - 그 그룹 내에는 OR만 존재, 모두 false 여야 false
             if (toDoAndOrList[i] == 0)
                 andIndex.push(i);
+        }
+
+        if (andIndex.length == 0) { //전부 OR일 때
+            for (var i = 0; i < searched.length; i++) {
+                if (searched[i] == false)
+                    _subMeet = false;                   
+            }
+            return _subMeet;
         }
 
         for (var i = 0; i < andIndex.length; i++) {
