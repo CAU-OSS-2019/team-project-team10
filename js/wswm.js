a6b72c44aa7d2d74606a3ba88fa7b3e88b427939
Felix Seol <felixxseol@gmail.com>
2019-06-05 18:37:19 +0900

    Map relocate to newCenter when wswm processed.

--- js/wswm.js
+++ js/wswm.js
@@ -6,6 +6,7 @@ var newCenter = [];
 
 function wswm() {
     console.log(result);
+    map.setLevel(7);
     var center = findCenter(); // 사용자들의 중간 위치 반환.
     //var center = [37.509218, 126.963359]; //test 를 위한 임시 center
     var a = wswm2(center).then(newCenter => {
@@ -13,8 +14,8 @@ function wswm() {
         resultMiddlePoint = newCenter;
         console.log(resultPlace);
         makeMarker(resultPlace);
+        map.setLevel(7);
     });
-
 }
 
 async function wswm2(cen) {
@@ -26,6 +27,9 @@ async function wswm2(cen) {
     console.log(cen);
 
     var coordinate = cen[0] + "," + cen[1];
+    let point = new daum.maps.LatLng(cen[1], cen[0]);
+    map.panTo(point);
+    sleep(500);
 
     for (var i = 0; i < toDoList.length; i++) { // 할일 목록인 toDoList가 있다고 가정 예 ) toDoList = ["삼겹살", "노래방", "방탈출카페", "당구장"]
         var plac = toDoList[i];
@@ -82,4 +86,8 @@ async function wswm2(cen) {
         }
         return wswm2(newCenter); // 이 방식을 재귀적으로 구현. 최종적으로 모든 list가 포함된 center값이 return 된다.
     }
+}
+
+function sleep(t) {
+    return new Promise(resolve=>setTimeout(resolve, t));
 }
